5dfc05b44b7d1f55b6c525606aa28fa9240082db
Alexander Karamushko <a.morgoth.b@gmail.com>
2020-05-13 23:15:12 +0300

    build(redux): delete store folder

--- src/store/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { numberCollectionReducer } from './test/reducers';
-
-export const rootReducer = combineReducers({
-    numberCollectionReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
