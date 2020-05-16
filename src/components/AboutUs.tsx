7e6bf24b790fba947d9d92d0005388457c58ebc7
Max <mk9977777799@mail.ru>
2020-05-16 15:02:55 +0300

    build(add-components) add components for test

---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class AboutUs extends React.Component {
+    render() {
+        return (
+            <h1>О нас</h1>
+        )
+    }
+}
+
+export default AboutUs;
---
+++ src/components/Contacts.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class Contats extends React.Component {
+    render() {
+        return (
+            <h1>Контакты</h1>
+        )
+    }
+}
+
+export default Contats;
