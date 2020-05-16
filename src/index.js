1b5a1c0f2efd0f13cbcf19b7e58377d4ccf9cea5
Max <mk9977777799@mail.ru>
2020-05-16 15:02:16 +0300

    add routing for react-app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './components/App';
+import AboutUs from './components/AboutUs';
+import Contacts from './components/Contacts';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+
+import {
+    BrowserRouter  as Router,
+    Route,
+    Switch,
+    Link,
+} from 'react-router-dom';
 
 import { rootReducer } from './store';
 
@@ -11,7 +21,33 @@ const store = createStore(rootReducer);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+
+        <Router>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>Главная</Link>
+                    </li>
+                    <li>
+                        <Link to='/about-us'>О нас</Link>
+                    </li>
+                    <li>
+                        <Link to='/contacts'>Контакты</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path='/about-us'>
+                    <AboutUs />
+                </Route>
+                <Route path='/contacts'>
+                    <Contacts />
+                </Route>
+                <Route path='/'>
+                    <App />
+                </Route>
+            </Switch>
+        </Router>
     </Provider>,
     document.getElementById('root'),
 );
