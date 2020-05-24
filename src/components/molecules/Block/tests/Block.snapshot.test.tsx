318fd60e70065c961dda8d906617dd013da29871
Alexander Karamushko <a.morgoth.b@gmail.com>
2020-05-24 15:46:02 +0300

    build(tests): add snapshot test

---
+++ src/components/molecules/Block/tests/Block.snapshot.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Block } from '../';
+
+describe('>>>B L O C K --- Snapshot', () => {
+    it('+++capturing Snapshot of Block', () => {
+        const renderedValue = renderer.create(<Block title="Test" />).toJSON();
+        expect(renderedValue).toMatchSnapshot();
+    });
+});
