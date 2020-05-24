ccc42c391c886666f98e0715bdada17a4012b411
Alexander Karamushko <a.morgoth.b@gmail.com>
2020-05-24 16:15:28 +0300

    build(test): add reducer action test

--- src/tests/number-collection.reducer.test.ts
+++ src/tests/number-collection.reducer.test.ts
@@ -9,4 +9,13 @@ describe('With snapshots ', () => {
 
         expect(numberCollectionReducer(undefined, action)).toMatchSnapshot();
     });
+
+    it('+++ reducer for GET_NUMBER_REQUEST_COMPLETED', () => {
+        let state = [0];
+        state = numberCollectionReducer(state, {
+            payload: 1,
+            type: '[1] NumberGenerator async service returned a new number.',
+        });
+        expect(state).toEqual([1]);
+    });
 });
