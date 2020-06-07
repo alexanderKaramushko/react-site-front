efb793e4a333bb10f24a4be279361d6c675fd7cf
Alexander Karamushko <a.morgoth.b@gmail.com>
2020-06-07 23:53:49 +0300

    feat(header): add settings menu

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,30 @@ import { store } from './store';
 import { routesArr, createRoutes } from './routes';
 import Header from './components/organisms/Header/header.container';
 
+const ThemeContext = React.createContext('');
+
 const App: React.FunctionComponent = () => (
     <Provider store={store}>
         <Router>
-            <Header />
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Главная</Link>
-                    </li>
-                    <li>
-                        <Link to="/about-us">О нас</Link>
-                    </li>
-                    <li>
-                        <Link to="/contacts">Контакты</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Switch>
-                {createRoutes(routesArr)}
-            </Switch>
+            <ThemeContext.Provider value={store.getState().themeReducer}>
+                <Header />
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Главная</Link>
+                        </li>
+                        <li>
+                            <Link to="/about-us">О нас</Link>
+                        </li>
+                        <li>
+                            <Link to="/contacts">Контакты</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    {createRoutes(routesArr)}
+                </Switch>
+            </ThemeContext.Provider>
         </Router>
     </Provider>
 );
--- src/components/organisms/Header/header.component.tsx
+++ src/components/organisms/Header/header.component.tsx
@@ -1,16 +1,20 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import Toggle from 'react-toggle';
+import Tippy from '@tippyjs/react';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
+import { ThemeType } from '../../../common/settings';
 
 import LabeledImage from '../../molecules/LabeledImage';
 import Label from '../../atoms/Label';
 
 import Logo from '../../../assets/icons/design.svg';
+import SettingsIcon from '../../../assets/icons/settings.svg';
 
 import 'react-toggle/style.css';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/shift-toward.css';
 import * as styles from './style.scss';
-import { ThemeType } from '../../../common/settings';
 
 interface Props {
     themeName: ThemeType;
@@ -24,28 +28,55 @@ const defaultProps = {
 const Header: React.FunctionComponent<Props> = (props) => {
     const { themeName, toggleTheme } = props;
     const classProps = classnames(styles.header, styles[themeName]);
+    const tipClassProps = classnames(styles[themeName], styles.tip);
+
+    const [visible, setVisible] = useState(false);
 
     function handleCHange(event: ChangeEvent<HTMLInputElement>): void {
         const { target } = event;
         toggleTheme(target.checked ? 'dark' : 'light');
     }
 
+    const show = (): void => setVisible(true);
+    const hide = (): void => setVisible(false);
+
     return (
         <header className={classProps}>
             <Link to="/">
                 <LabeledImage imagePosition="left" label="Logo" theme={themeName}>
-                    <Logo className="design_svg__animated" width="50px" />
+                    <Logo className="design_svg__animated" width="40px" />
                 </LabeledImage>
             </Link>
 
-            <div className={styles.themeToggler}>
-                <Label theme={themeName} size="small">Night mode:</Label>
-                <Toggle
-                    onChange={handleCHange}
-                    icons={false}
-                    defaultChecked={false}
-                />
-            </div>
+            <Tippy
+                content={(
+                    <div>
+                        <div className={styles.themeToggler}>
+
+                            <Label theme={themeName} size="small">Night mode:</Label>
+                            <Toggle
+                                onChange={handleCHange}
+                                icons={false}
+                                defaultChecked={false}
+                            />
+                        </div>
+                    </div>
+                )}
+                animation="shift-toward"
+                className={tipClassProps}
+                interactive
+                onClickOutside={hide}
+                visible={visible}
+            >
+                <span>
+                    <SettingsIcon
+                        className={styles.settingsIcon}
+                        height="40px"
+                        onClick={visible ? hide : show}
+                        width="40px"
+                    />
+                </span>
+            </Tippy>
         </header>
     );
 };
