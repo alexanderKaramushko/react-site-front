e94dcf30f2773f9ce860a974a8d83c41e11f4027
Max <mk9977777799@mail.ru>
2020-06-22 21:17:44 +0300

    fix(any) add return type for arguments in connect-function

--- src/components/organisms/Header/header.container.ts
+++ src/components/organisms/Header/header.container.ts
@@ -9,12 +9,22 @@ import Header from './header.component';
 import { ThemeType } from '../../../common/settings';
 import { RootState } from '../../../reducers';
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
+type mapDispatchReturn = {
+    setLocaleWithFallback: (desiredLocale: string) => void;
+    toggleTheme: (themeName: ThemeType) => ThemeAction;
+}
+
+const mapDispatchToProps = (dispatch: Dispatch): mapDispatchReturn => ({
     setLocaleWithFallback: setLocaleWithFallback(dispatch),
     toggleTheme: (themeName: ThemeType): ThemeAction => dispatch(toggleThemeAction(themeName)),
 });
 
-const mapStateToProps = (state: RootState) => ({
+type MapStateReturn = {
+    selectedLocale: string;
+    theme: ThemeType;
+}
+
+const mapStateToProps = (state: RootState): MapStateReturn => ({
     selectedLocale: getSelectedLocale(state),
     theme: getActiveTheme(state),
 });
