32b43803f5cb2f88668b948b8d66ef059c0f3eba
Max <52855511+MaxKaram@users.noreply.github.com>
2020-08-04 22:14:06 +0300

    build(backend) change database (#25)
    upd(backend): replace temporarily database connection with json data

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,11 @@
 // @ts-nocheck
 const express = require('express');
-const Sequelize = require('sequelize');
-const createUserModel = require('./models/User').createModel;
-const createContactModel = require('./models/Contact').createModel;
-
-const sequelize = new Sequelize('reactdb', 'root', '', {
-    define: {
-        timestamps: false,
-    },
-    dialect: 'mysql',
-    host: '192.168.0.14',
-    logging: false,
-});
-
-const User = createUserModel(Sequelize, sequelize);
-const Contact = createContactModel(Sequelize, sequelize);
-
-User.hasMany(Contact);
+const fs = require('fs');
 
 const app = express();
-
-// // const jsonParser = express.json();
-
 app.get('/api/main', (req, res) => {
-    User.findAll().then((users) => {
-        const answer = [];
-        const promises = [];
-        for (let i = 0; i < users.length; i += 1) {
-            answer.push({
-                description: users[i].description,
-                id: users[i].id,
-                name: users[i].name,
-            });
-            promises.push(users[i].getContacts().then((c) => { answer[i].contacts = c; }));
-        }
-
-        Promise.all(promises).then(() => {
-            res.send(answer);
-        });
+    fs.readFile('./backend/data.json', 'utf8', (err, data) => {
+        res.send(data);
     });
 });
 
