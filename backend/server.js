bf7f2695542632243760f20caeed612d08abb132
Max <mk9977777799@mail.ru>
2020-08-08 21:29:12 +0300

    feat(backend) add new request handler

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,18 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+
 app.get('/api/main', (req, res) => {
     fs.readFile('./backend/data.json', 'utf8', (err, data) => {
-        res.send(data);
+        const answer = JSON.parse(data).users;
+        res.send(JSON.stringify(answer));
+    });
+});
+
+app.get('/api/main/processes', (req, res) => {
+    fs.readFile('./backend/data.json', 'utf8', (err, data) => {
+        const answer = JSON.parse(data).processes;
+        res.send(JSON.stringify(answer));
     });
 });
 
