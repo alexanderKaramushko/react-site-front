a8cbb95ce4f494af92c23573db718c4fa7dc80ff
Александр Карамушко <53118828+alexanderKaramushko@users.noreply.github.com>
2020-08-09 22:06:09 +0300

    Scale text size (#27)
    * fix(path) fix path in import
    
    * build(add) add TextSize component
    
    * refactor(font-size) change px on rem
    
    * feat(localization) add new word in localization
    
    * build(action) add new action for TextSize
    
    * build(selectors) add selector for TextSize
    
    * build(reducer) add reducer for TextSize
    
    * build(state) change state type
    
    * test(state) change init state in test
    
    * feat(settings) add TextSize component in header
    
    * fix(settings): add review fixes
    
    Co-authored-by: Max <mk9977777799@mail.ru>

--- src/store/reducers/settings/actions.ts
+++ src/store/reducers/settings/actions.ts
@@ -2,3 +2,4 @@ import { createAction } from 'typesafe-actions';
 import { Themes } from '../../../common/settings';
 
 export const toggleThemeAction = createAction('TOGGLE_THEME', (themeName: Themes) => themeName)<Themes>();
+export const toggleTextSizeAction = createAction('TOGGLE_TEXT_SIZE', (size: number) => size)<number>();
