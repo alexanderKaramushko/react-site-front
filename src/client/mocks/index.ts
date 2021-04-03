5f5e1631930b37b80a26196e41188968e6e8c882
Max <mk9977777799@mail.ru>
2021-04-03 18:37:04 +0300

    refactor(tests) delete all tests

--- src/client/mocks/index.ts
+++
@@ -1,8 +0,0 @@
-import configureStore, { MockStoreEnhanced } from 'redux-mock-store';
-
-export function mockStore<T>(initialState: T): MockStoreEnhanced {
-  const configureMockStore = configureStore();
-  const store = configureMockStore(initialState);
-
-  return store;
-}
