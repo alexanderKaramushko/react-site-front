8e8a9dae2d5f920b877037950f6c092db17030e6
alex.karamushko <alex.karamushko@nordclan.com>
2021-04-04 15:26:18 +0300

    feat(auth): intercept request to enrich it with bearer token

--- src/client/api/sagas/authenticate.ts
+++ src/client/api/sagas/authenticate.ts
@@ -1,24 +1,22 @@
 import { takeEvery, call, put } from 'redux-saga/effects';
 import { SagaIterator } from 'redux-saga';
-import jwtDecode from 'jwt-decode';
 
 import { AuthResponse } from '../AuthService/types';
 import { AUTHENTICATION_TYPES } from '../../store/reducers/authentication/types';
 import { AuthenticationAsyncRequest } from '../../store/reducers/authentication/actions.types';
-import { UserFromJwt } from '../../models/base/UserFromJwt';
+
+import { setAccessToken } from '../utils';
 
 import { AuthService } from '..';
 import { authenticateAsync } from '../../store/reducers/authentication/actions';
-import { updateUser } from '../../store/reducers/user/actions';
 
 const authService = new AuthService();
 
 export function* authenticate(action: ReturnType<AuthenticationAsyncRequest>): SagaIterator {
   try {
     const response: AuthResponse = yield call(authService.authenticate, action.payload);
-    const { username } = jwtDecode<UserFromJwt>(response.access_token);
 
-    yield put(updateUser({ username }));
+    setAccessToken(response.access_token);
 
     // todo remove nulls
     yield put(authenticateAsync.success(null, null));
